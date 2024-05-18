92d7ccdb95fb528f181c7d5dd80ed282d7414618
Tadeo <tadeojcocucci@gmail.com>
2024-05-18 20:15:10 -0300

    feat: password validation

--- src/schemas.py
+++ src/schemas.py
@@ -25,6 +25,22 @@ class UserRequestModel(BaseModel):
             raise ValueError("Username must be between 3 and 50 characters")
         return username
 
+    @validator("password")
+    def validate_password(cls, password):
+        if len(password) < 8 or len(password) > 20:
+            raise ValueError("Password must be between 8 and 20 characters")
+        if not any(char.islower() for char in password):
+            raise ValueError("Password must contain at least one lowercase letter")
+        if not any(char.isupper() for char in password):
+            raise ValueError("Password must contain at least one uppercase letter")
+        if not any(char.isdigit() for char in password):
+            raise ValueError("Password must contain at least one number")
+        if not any(char in "@#$^&*+=!." for char in password):
+            raise ValueError(
+                "Password must contain at least one special character (@#$^&*+=)"
+            )
+        return password
+
 
 class UserResponseModel(BaseModel):
     id: int
