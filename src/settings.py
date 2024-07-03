33fe5804166eb6c38a23f60cf85a9e29f3927929
Tadeo <tadeojcocucci@gmail.com>
2024-07-03 08:28:56 -0300

    fix: cors

--- src/settings.py
+++ src/settings.py
@@ -10,5 +10,6 @@ DATABASE_PASS = os.environ.get("DATABASE_PASS")
 DATABASE_HOST = os.environ.get("DATABASE_HOST")
 DATABASE_PORT = int(os.environ.get("DATABASE_PORT_CONTAINER"))
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS").split(" ")
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 24 * 60
